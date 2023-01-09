91273425162484058263b9469d568657ecef3c97
Franck-Noubissi <younesselghazi2000@gmail.com>
2023-01-09 12:03:13 +0100

    Formatage OK

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 //var express = require('express');
 import express from "express";
+import { contacts } from "./data.js";
+import * as controller from "./controller.js";
 var app = express();
-
-const contacts = [{
-  id: "0",
-  nom: "Xavier",
-  telephone: "047U3894894"
-},
-{
-  id: "1",
-  nom: "Tulipe",
-  telephone: "78499387493",
-}]
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-app.get('/api/contacts', function (req, res) {
-  res.status(200).json(contacts)
-});
+app.get('/api/contacts', controller.getContacts);
 
-app.get('/api/contact/:id', function (req, res) {
-  const id = req.params.id;
-  var contact = contacts.find((user) => user.id === id)
-  res.status(200).json(contact)
-})
+app.get('/api/contact/:id', controller.getContactById)
 
 //module.exports = app
 export default app
---
+++ data.js
@@ -0,0 +1,10 @@
+export const contacts = [{
+    id: "0",
+    nom: "Xavier",
+    telephone: "047U3894894"
+},
+{
+    id: "1",
+    nom: "Tulipe",
+    telephone: "78499387493",
+}]
